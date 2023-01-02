ca5c12f9bb9f2c4291dad86e0625acb87d4b00ab
Vladimir Nybozhinsky <vladimir.nybozhinsky@quickblox.com>
2023-01-02 23:31:34 +0200

    Part#5-DialogsScreen-DialogView

---
+++ Articles/IOS/QuickbloxSwiftUIChat/QuickbloxSwiftUIChat/Screens/ChatScreen/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  QuickbloxSwiftUIChat
+//
+//  Created by Injoit on 02.01.2023.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text("ChatView!")
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
--- Articles/IOS/QuickbloxSwiftUIChat/QuickbloxSwiftUIChat/Screens/DialogsScreen/DialogsView.swift
+++ Articles/IOS/QuickbloxSwiftUIChat/QuickbloxSwiftUIChat/Screens/DialogsScreen/DialogsView.swift
@@ -11,8 +11,13 @@ import UIKit
 
 struct DialogsView: View {
     
+    init() {
+        //MARK: Disable selection style.
+        UITableViewCell.appearance().selectionStyle = .none
+    }
+    
     @Environment(\.presentationMode) var presentationMode
-    @State var dialogs: [QBChatDialog] = []
+    @State var dialogs: [Dialog] = []
     let quickBloxServerAPI = QuickBloxServerAPI()
     
     var body: some View {
@@ -21,24 +26,57 @@ struct DialogsView: View {
             Color.clear
                 .overlay(
                     List {
-                        ForEach(dialogs, id: \.self) { dialog in
-                            Text(dialog.name ?? "TestDialog" )
+                        ForEach(dialogs) { dialog in
+                            ZStack {
+                                DialogView(dialog: dialog)
+                                NavigationLink(destination: ChatView()) {
+                                    EmptyView()
+                                }
+                                .frame(width: 0)
+                                .opacity(0)
+                            }
+                            .padding(.vertical, 4)
+                            .padding(.leading, -3)
+                            .listRowSeparator(.hidden)
                         }
+                        .onDelete(perform: delete)
                     }
                         .listStyle(PlainListStyle())
+                        .refreshable {
+                            self.fetchDialogs()
+                        }
                         .onAppear() {
                             self.fetchDialogs()
+                            UITableView.appearance().allowsSelection = true
+                            UITableViewCell.appearance().selectionStyle = .none
                         }
                 )}
         
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: Button(action: { didTapLogout() }) {
+            Image(uiImage: UIImage(named: "exit") ?? UIImage())
+        })
         .navigationBarTitle("Chats", displayMode: .inline)
+        .navigationBarItems(trailing: Button(action: {}) {
+            Image(uiImage: UIImage(named: "add") ?? UIImage())
+        })
+        .navigationBarItems(trailing: Button(action: {}) {
+            Image(uiImage: UIImage(named: "icon-info") ?? UIImage())
+        })
     }
     
-    func fetchDialogs() {
+    private func didTapLogout() {
+        if QBChat.instance.isConnected == false {
+            return
+        }
+        self.presentationMode.wrappedValue.dismiss()
+    }
+    
+    private func fetchDialogs() {
         quickBloxServerAPI.fetchDialogs { result in
             switch result {
             case .success(let dialogs):
-                guard let dialogs = dialogs else { return }
+                if dialogs.isEmpty { return }
                 DispatchQueue.main.async {
                     self.dialogs = dialogs
                 }
@@ -46,6 +84,10 @@ struct DialogsView: View {
                 debugPrint("[DialogsView] \(#function) error: \(error.localizedDescription)")
             }
         }
+    }
+    
+    private func delete(at offsets: IndexSet) {
+        self.dialogs.remove(atOffsets: offsets)
     }
 }
 
